c5f284d45723742e3bf8722a0f535ee07079d204
viyan-najmaldin <najmadinviyan@gmail.com>
2024-02-19 11:35:26 +0300

    tanha bo testa #

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php  
+$color ="sur";
+
+switch($color){
+case 'sur':
+    echo "stop";
+    break;
+
+    case 'zard':
+        echo "la amadabashi ba";
+        break;
+
+     case 'sawz':
+            echo "bro";
+            break;
+    
+    default :
+     echo "chawarey mrur bka";
+}
+
+
+if ($name == "ahmad")
+ echo "mn ahmadm";
+
+ else if($name == "tawar"){
+    echo "mn tawarm";
+ }
+
+ else if($name == "chra"){
+    echo "mn chram";
+ }
+
+ else if($name == "hana"){
+    echo "mn hanam";
+ }
+
+else {
+    echo "mn nawm nya lera ";
+}
+
+
+?>
